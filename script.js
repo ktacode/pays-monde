4497398786e92bf0f07f554d089611fec553f491
pbokolo <dev.pbokolo@gmail.com>
2024-11-28 15:40:08 +0200

    Code refactoring: getting rid of useless codes

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 "use strict";
 
 const API = `https://restcountries.com/v3.1`;
-const COUNTRY_CARD_MARKUP = `<div class="card">
-        <div class="card__img"></div>
-        <div class="card__details">
-          <p class="text text--md text--bold">DR Congo</p>
-          <p class="text text--sm text--secondary">
-            <span class="material-icons">location_city</span>&nbsp;Kinshasa
-          </p>
-          <p class="text text--sm text--secondary">
-            <span class="material-icons">south_america</span>&nbsp;Africa
-          </p>
-          <p class="text text--sm text--secondary">
-            <span class="material-icons">diversity_3</span>&nbsp;100 M
-          </p>
-          <p class="text text--sm text--secondary">
-            <span class="material-icons">attach_money</span>&nbsp;Congolese
-            Franc
-          </p>
-        </div>
-      </div>`;
 
 const searchForm = document.querySelector(".search-form");
 const section = document.querySelector(".section");
