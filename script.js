364eed42c6c3b31c117673dedf4b11c96af3359b
pbokolo <dev.pbokolo@gmail.com>
2024-11-28 06:26:10 +0200

    Initializing the app script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const API = `https://restcountries.com/v3.1`;
+
+async function getAllCountries() {
+  try {
+    const result = await fetch(`${API}/all`);
+    const data = await result.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function renderCountries(list) {}
+
+function start() {
+  getAllCountries();
+}
+
+start();
